3b9e9d3f2cbfaff904a6f85c982bef68a100f845
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-04 16:15:12 +0200

    Перше завдання Гуд

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,64 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", handleGaleryContainerClick);
+function handleGaleryContainerClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const modalImage = event.target.dataset.source;
+  console.log(event.modalImage);
+
+  const instance = basicLightbox.create(`
+<img
+src="${modalImage}"
+width="800"
+height="600" />
+`);
+
+  instance.show();
+}
+
+
+// function createGalleryCardsMarkup(array) {
+//   return array.map(({ preview, original, description }) => {
+//     const item = document.createElement("a");
+//     item.href = original;
+//     item.classList.add("gallery__item");
+//     item.dataset.caption = description;
+//     const image = document.createElement("img");
+//     image.src = preview;
+//     image.classList.add("gallery__image");
+//     image.alt = description;
+//     image.title = description;
+//     image.delay = 250;
+//     image.titlePosition = "top";
+//     item.append(image);
+//     return item;
+//   });
+// }
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.setAttribute("uk-lightbox", "caption-position:bottom");
+console.log(galleryContainer);
+galleryContainer.addEventListener("click", handleGaleryContainerClick);
+
+function createGalleryCardsMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}" data-caption = "${description}";
+        >
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title = ${description};
+            delay = 250; 
+            titlePosition = "top"/>
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+
+const galleryElements = createGalleryCardsMarkup(galleryItems);
+galleryContainer.append(...galleryElements);
+function handleGaleryContainerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(event.target);
+  let href = event.target.closest("a").getAttribute("href");
+  return href;
+}
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {});
+gallery.on("error.simplelightbox", function (event) {
+  console.log(event);
+});
+
+console.log(createGalleryElement(galleryItems));
